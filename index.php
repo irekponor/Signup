179c9165245196dbd7556acd5e034ccee36e7441
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-07 00:18:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group">
+                <input type="text" name="fullname" placeholder="Full Name">
+            </div>
+            <div class="form-group">
+                <input type="text" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="password" name="repeat_password" placeholder="Repeat Password">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Register" name="submit">
+            </div>
+        </form>
+    </div>
+    </form>
+
+</body>
+
+</html>
