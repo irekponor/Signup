cad5ee3447d62eb2158181e8fcd6a48b0acb1351
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-07 00:37:19 +0100

    commit

--- index.php
+++ index.php
@@ -14,19 +14,19 @@
     <div class="container">
         <form action="" method="post">
             <div class="form-group">
-                <input type="text" name="fullname" placeholder="Full Name">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name">
             </div>
             <div class="form-group">
-                <input type="text" name="email" placeholder="Email">
+                <input type="text" class="form-control" name="email" placeholder="Email">
             </div>
             <div class="form-group">
-                <input type="password" name="password" placeholder="Password">
+                <input type="password" class="form-control" name="password" placeholder="Password">
             </div>
             <div class="form-group">
-                <input type="password" name="repeat_password" placeholder="Repeat Password">
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repeat Password">
             </div>
             <div class="form-group">
-                <input type="submit" value="Register" name="submit">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
             </div>
         </form>
     </div>
