5665a1b5c2213290719c1514efbbebf022f7287f
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-07 03:03:18 +0100

    new

--- registration.php
+++ registration.php
@@ -5,4 +5,14 @@ if (isset($_POST["submit"])) {
     $email = $_POST["email"];
     $pwd = $_POST["pwd"];
     $pwdRepeat = $_POST["repeat_pwd"];
+    $errors = array();
+    if (empty($fullname) or empty($email) or empty($pwd) or empty($pwdRepeat)) {
+        array_push($errors, "All fileds are required, fool!.");
+    }
+    if (!filter_var($email, FILTER_VALIDATE_IP)) {
+        array_push($errors, "who are you deceiving? c'mon use a valid Email");
+    }
+    if (strlen($pwd) < 8) {
+        array_push($errors, "Passwords must be at least 8 chars long, idiot.");
+    }
 }
