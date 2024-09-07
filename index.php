0d47caf9daebca62c0a98da7a6c2c8db450f20fd
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-08 00:24:47 +0100

    yes

--- index.php
+++ index.php
@@ -42,6 +42,39 @@
                 $errors = true;
             }
         }
+
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $fullname = $_POST["fullname"];
+            $email = $_POST["email"];
+            $pwd = $_POST["pwd"];
+            $pwdRepeat = $_POST["repeat_pwd"];
+
+            try {
+                require_once "database.php";
+
+                $query = "INSERT INTO users (fullname, email, pwd) VALUES (:fullname, :email, :pwd);";
+
+                $stmt = $pdo->prepare($query);
+
+                $stmt->bindParam(":fullname", $fullname);
+                $stmt->bindParam(":email", $email);
+                $stmt->bindParam(":pwd", $pwd);
+
+                $stmt->execute();
+
+                $pdo = null;
+                $stmt = null;
+
+                header("Location: index.php");
+
+                die();
+            } catch (PDOException $e) {
+                die("Query Failed:" . $e->getMessage());
+            }
+        } else {
+            header("Location: index.php");
+        }
         ?>
         <form action="index.php" method="post">
             <div class="form-group">
