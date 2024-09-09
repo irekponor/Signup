980786bb20f84b82b4a3420bafdc2517477ac6f3
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-10 00:49:11 +0100

    home

--- delete-user.php
+++ delete-user.php
@@ -16,67 +16,12 @@
 
             <?php
 
-            if (isset($_POST["submit"])) {
-                $email = $_POST["email"];
-                $pwd = $_POST["pwd"];
-                $pwdRepeat = $_POST["repeat_pwd"];
-
-                $errors = false;
-
-
-                if (empty($email) or empty($pwd) or empty($pwdRepeat)) {
-                    echo "<div class='alert alert-danger'>All fields are required!.</div>";
-                    $errors = true;
-                } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    echo "<div class='alert alert-danger'>Email not found.</div>";
-                    $errors = true;
-                } elseif (strlen($pwd) < 8) {
-                    echo "<div class='alert alert-danger'>Enter a correct password.</div>";
-                    $errors = true;
-                } elseif ($pwd !== $pwdRepeat) {
-                    echo "<div class='alert alert-danger'>Passwords don't match!.</div>";
-                    $errors = true;
-                } else {
-                    echo "<div class='alert alert-success'>Account deleted succesfully!</div>";
-                    echo "<a href='index.php'>Home</a>";
-                    $errors = true;
-                }
-            }
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $email = $_POST["email"];
-                $pwd = $_POST["pwd"];
-                $pwdRepeat = $_POST["repeat_pwd"];
-
-                try {
-                    require_once "database.php";
-
-                    $query = "DELETE FROM users WHERE email = :email AND pwd = :pwd;";
-
-                    $stmt = $pdo->prepare($query);
-
-                    $stmt->bindParam(":email", $email);
-                    $stmt->bindParam(":pwd", $pwd);
-
-                    $stmt->execute();
-
-                    $pdo = null;
-                    $stmt = null;
-
-
-                    die();
-                } catch (PDOException $e) {
-                    die("Query Failed:" . $e->getMessage());
-                }
-            }
             ?>
 
             <input type="text" name="email" placeholder="Email">
 
 
             <input type="password" name="pwd" placeholder="Password">
-
-
-            <input type="password" name="repeat_pwd" placeholder="Repeat Password">
 
 
             <input type="submit" class="btn btn-danger" value="Delete" name="submit">
