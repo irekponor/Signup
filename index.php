bcc0069f519ef0470f3ed61640afe26be57ade5d
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-09 03:46:32 +0100

    comm

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="https://kit.fontawesome.com/de8e2530fe.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -77,6 +78,7 @@
 
         <form action="index.php" method="post">
 
+            <i class="fa-solid fa-user"></i>
             <input type="text" name="fullname" placeholder="Full Name">
 
             <input type="text" name="email" placeholder="Email">
